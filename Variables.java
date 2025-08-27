3122b025c4d4521522fceb8e6c04264c54405195
MikuUchida <mikuuchida@mac>
2025-08-27 18:21:47 +0900

    変数

---
+++ Variables.java
@@ -0,0 +1,26 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+        String myfavorite= "私の好きな";
+        String integer = "整数は";
+        String decimal ="少数は";
+        String last = "です。";
+        int number=8;
+        double num= 0.114;
+        
+        System.out.println(number);
+        System.out.println(num);
+        System.out.println(myfavorite+integer+number+last);
+        System.out.println(myfavorite+decimal+num+last);
+        
+        
+    
+        
+		
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
