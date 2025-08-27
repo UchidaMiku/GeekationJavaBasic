d88c2afea1ee74b6cc4e61633032ba48ee9da9b7
MikuUchida <mikuuchida@mac>
2025-08-27 21:40:58 +0900

    変数

---
+++ Calculation.java
@@ -0,0 +1,30 @@
+package basicSyntax;
+
+public class Calculation {
+
+	public static void main(String[] args) {
+		String number= "変数";
+		String figure= "の値は";
+		String last = "です。";
+		
+				
+		int x = 5;
+		int y = 10;
+
+		double a= 5.25;
+		int b = 10;
+		
+		a+= 2.5;
+		a*= 3.0;
+		
+		System.out.println(number+"z"+figure+(x+y)+last);
+		System.out.println(number+"a"+figure+a+last);
+		System.out.println(number+"b"+figure+((int)a+b)+last);
+		
+		
+	
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
